fd0df1c181d7a180927270bea07f5ed9bc629687
Piergiuliano Bossi <pgbossi@gmail.com>
2014-01-03 15:14:56 -0500

    Test constants don't really need to be self instances.

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -20,29 +20,30 @@ class TestLine(object):
         eq_(0.5, l.value(2))
 
 
+RAMP_QUANTITY = 10
+FULL_LOAD_QUANTITY = 20
+INITIAL_DELAY = 1
+FULL_LOAD_DELAY = 0.5
+TOTAL_QUANTITY = RAMP_QUANTITY * 2 + FULL_LOAD_QUANTITY
+
 class TestSchedule(object):
-    RAMP_QUANTITY = 10
-    FULL_LOAD_QUANTITY = 20
-    INITIAL_DELAY = 1
-    FULL_LOAD_DELAY = 0.5
-    TOTAL_QUANTITY = RAMP_QUANTITY * 2 + FULL_LOAD_QUANTITY
     def setup(self):
-        self.schedule = Schedule(self.RAMP_QUANTITY, self.FULL_LOAD_QUANTITY, self.INITIAL_DELAY, self.FULL_LOAD_DELAY)
-        self.values = [self.schedule.next(index) for index in range(self.TOTAL_QUANTITY)]
+        self.schedule = Schedule(RAMP_QUANTITY, FULL_LOAD_QUANTITY, INITIAL_DELAY, FULL_LOAD_DELAY)
+        self.values = [self.schedule.next(index) for index in range(TOTAL_QUANTITY)]
 
     def _ramp_up(self):
-        delay_increment = (self.INITIAL_DELAY - self.FULL_LOAD_DELAY) / self.RAMP_QUANTITY
-        return sum(self.INITIAL_DELAY - i * delay_increment for i in range(self.RAMP_QUANTITY))
+        delay_increment = (INITIAL_DELAY - FULL_LOAD_DELAY) / RAMP_QUANTITY
+        return sum(INITIAL_DELAY - i * delay_increment for i in range(RAMP_QUANTITY))
 
     def test_full_load_after_ramp_up_shows_constant_rate(self):
-        assert_almost_equal(self.FULL_LOAD_QUANTITY * self.FULL_LOAD_DELAY, self.schedule.full_load - self.schedule.ramp_up)
-        for i in range(self.RAMP_QUANTITY, self.RAMP_QUANTITY + self.FULL_LOAD_QUANTITY):
-            assert_almost_equal(self.FULL_LOAD_DELAY, self.values[i] - self.values[i - 1])
+        assert_almost_equal(FULL_LOAD_QUANTITY * FULL_LOAD_DELAY, self.schedule.full_load - self.schedule.ramp_up)
+        for i in range(RAMP_QUANTITY, RAMP_QUANTITY + FULL_LOAD_QUANTITY):
+            assert_almost_equal(FULL_LOAD_DELAY, self.values[i] - self.values[i - 1])
 
     def test_ramp_up_and_ramp_down_are_anti_symmetrical(self):
         assert_almost_equal(self._ramp_up(), self.schedule.ramp_up)
         assert_almost_equal(self.schedule.ramp_up + self.schedule.full_load, self.schedule.ramp_down)
-        for i in range(self.RAMP_QUANTITY - 1):
-            assert_almost_equal(self.values[self.RAMP_QUANTITY - i - 1] - self.values[self.RAMP_QUANTITY - i - 2], self.values[self.RAMP_QUANTITY + self.FULL_LOAD_QUANTITY + i] - self.values[self.RAMP_QUANTITY + self.FULL_LOAD_QUANTITY + i - 1], msg="index={}".format(i))
+        for i in range(RAMP_QUANTITY - 1):
+            assert_almost_equal(self.values[RAMP_QUANTITY - i - 1] - self.values[RAMP_QUANTITY - i - 2], self.values[RAMP_QUANTITY + FULL_LOAD_QUANTITY + i] - self.values[RAMP_QUANTITY + FULL_LOAD_QUANTITY + i - 1], msg="index={}".format(i))
 
     # TODO: show that it increments by 0.05 at each ramp step + show a simpler profile
